7a63ff000d771ccaed0b61ba7d868131d9232990
skylerrankin <skylerrankin@gmail.com>
2024-12-14 13:28:43 -0500

    Use window startup and remove gl debug logs in release builds

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,18 @@
 #include "glCommon.h"
 #include "res/icon64.h"
 
+#ifndef NDEBUG
 static void errorCallback(int error, const char* description) {
     std::cout << "GLFW error (" << error << "): " << description << std::endl;
 }
+
+static void debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+    if (id == 131185) return; // Buffer object X (bound to GL_ELEMENT_ARRAY_BUFFER_ARB, usage hint is GL_STREAM_DRAW) will use VIDEO memory as the source for buffer object operations.
+    if (id == 131154) return; // Pixel-path performance warning: Pixel transfer is synchronized with 3D rendering.
+    if (id == 131218) return; // Program/shader state performance warning: Vertex shader in program 3 is being recompiled based on GL state.
+    std::cout << std::format("GL Debug Message: source={} type={} id={} sev={} len={} msg={}", source, type, id, severity, length, message) << std::endl;
+}
+#endif
 
 static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     Application* app = static_cast<Application*>(glfwGetWindowUserPointer(window));
@@ -25,21 +34,6 @@ static void mousePositionCallback(GLFWwindow* window, double x, double y) {
 static void scrollCallback(GLFWwindow* window, double xOffset, double yOffset) {
     Application* app = static_cast<Application*>(glfwGetWindowUserPointer(window));
     app->onScroll((int) yOffset);
-}
-
-static void debugCallback(GLenum source,
-    GLenum type,
-    GLuint id,
-    GLenum severity,
-    GLsizei length,
-    const GLchar* message,
-    const void* userParam) {
-
-    if (id == 131185) return;
-    if (id == 131154) return; // Pixel-path performance warning: Pixel transfer is synchronized with 3D rendering.
-    if (id == 131218) return; // Program/shader state performance warning: Vertex shader in program 3 is being recompiled based on GL state.
-
-    std::cout << std::format("Debug message: source={} type={} id={} sev={} len={} msg={}", source, type, id, severity, length, message) << std::endl;
 }
 
 int main() {
@@ -64,17 +58,19 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwMakeContextCurrent(window);
-    glfwSetErrorCallback(errorCallback);
     glfwSetKeyCallback(window, &keyCallback);
     glfwSetMouseButtonCallback(window, &clickCallback);
     glfwSetCursorPosCallback(window, &mousePositionCallback);
     glfwSetScrollCallback(window, &scrollCallback);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
+#ifndef NDEBUG
+    glfwSetErrorCallback(errorCallback);
     glEnable(GL_DEBUG_OUTPUT);
     glDebugMessageCallback(debugCallback, nullptr);
-
     const GLubyte* version = glGetString(GL_VERSION);
     std::cout << "OpenGL version: " << version << std::endl;
+#endif
 
     Application app{window};
     glfwSetWindowUserPointer(window, &app);
